4ba48c977a3119ffeaf60c9bcac89a1ad1e91d2b
kitakaze-kan <kantaro.1013.fujimori@gmail.com>
2022-08-24 21:13:49 +0900

    fix: remove fee

--- contracts/BulkPayment.sol
+++ contracts/BulkPayment.sol
@@ -24,6 +24,8 @@ contract BulkPayment is Ownable, Pausable, ReentrancyGuard {
 
     uint private protocolFee = 875; // rate = protocolFee / 100000
 
+    uint private noFeeCharged = 1; // 0 = false : 1 = true
+
     /**
      * @notice Required parameters for each donation
      */
@@ -55,9 +57,7 @@ contract BulkPayment is Ownable, Pausable, ReentrancyGuard {
     );
 
     /**
-     * @notice Bulk gitcoin grant donations
-     * @dev We assume all token approvals were already executed
-     * @param _payments Array of donation structs
+     * @param _payments Array of payment structs
      */
     function pay(Payment[] calldata _payments)
         external
@@ -65,57 +65,106 @@ contract BulkPayment is Ownable, Pausable, ReentrancyGuard {
         nonReentrant
         whenNotPaused
     {
-        // We track total ETH donations to ensure msg.value is exactly correct
-        uint256 _ethDonationTotal = 0;
-
-        //protocol fee
-        uint feeRate = protocolFee + 100000;
-        uint totalAmount = (msg.value * 100000) / feeRate;
+        // We track total payments to ensure msg.value is exactly correct
+        uint256 totalAmount = 0;
 
         for (uint256 i = 0; i < _payments.length; i++) {
-            uint price = (_payments[i].amount * 100000) / feeRate;
+            uint price = _payments[i].amount;
             console.log("price", price);
 
             emit PaymentSent(
                 _payments[i].token,
-                price,
+                _payments[i].amount,
                 _payments[i].dest,
                 _payments[i].deliverable,
                 msg.sender
             );
 
             if (_payments[i].token != ETH_TOKEN_PLACHOLDER) {
-                // Token donation
+                // Token payment
                 // This method throws on failure, so there is no return value to check
-                
                 SafeERC20.safeTransferFrom(
                     IERC20(_payments[i].token),
                     msg.sender,
                     _payments[i].dest,
-                    price
-                );
-                SafeERC20.safeTransferFrom(
-                    IERC20(_payments[i].token),
-                    msg.sender,
-                    address(this),
-                    (_payments[i].amount - price)
+                    _payments[i].amount
                 );
             } else {
-                // ETH donation
-                // See comments in Address.sol for why we use sendValue over transer
-                _payments[i].dest.sendValue(price);
-                _ethDonationTotal = _ethDonationTotal + price;
+                // ETH payment
+                _payments[i].dest.sendValue(_payments[i].amount);
+                totalAmount = totalAmount + _payments[i].amount;
             }
         }
 
-        console.log("_ethDonationTotal", _ethDonationTotal);
+        console.log("totalAmount", totalAmount);
 
         // Revert if the wrong amount of ETH was sent
         require(
-            totalAmount == _ethDonationTotal,
+            msg.value == totalAmount,
             "bulkPayment: Too much ETH sent"
         );
     }
+
+    /**
+     * @param _payments Array of payment structs
+     */
+    // function payWithFee(Payment[] calldata _payments)
+    //     external
+    //     payable
+    //     nonReentrant
+    //     whenNotPaused
+    // {
+    //     // We track total payments to ensure msg.value is exactly correct
+    //     uint256 totalAmount = 0;
+
+    //     //protocol fee
+    //     uint feeRate = protocolFee + 100000;
+    //     uint total = (msg.value * 100000) / feeRate;
+
+    //     for (uint256 i = 0; i < _payments.length; i++) {
+    //         uint fee = (_payments[i].amount * 100000) / 10000;
+    //         uint price = _payments[i].amount - fee;
+    //         console.log("payWithFee:price", price);
+
+    //         emit PaymentSent(
+    //             _payments[i].token,
+    //             price,
+    //             _payments[i].dest,
+    //             _payments[i].deliverable,
+    //             msg.sender
+    //         );
+
+    //         if (_payments[i].token != ETH_TOKEN_PLACHOLDER) {
+    //             // Token payment
+    //             // This method throws on failure, so there is no return value to check
+                
+    //             SafeERC20.safeTransferFrom(
+    //                 IERC20(_payments[i].token),
+    //                 msg.sender,
+    //                 _payments[i].dest,
+    //                 price
+    //             );
+    //             SafeERC20.safeTransferFrom(
+    //                 IERC20(_payments[i].token),
+    //                 msg.sender,
+    //                 address(this),
+    //                 fee
+    //             );
+    //         } else {
+    //             // ETH payment
+    //             _payments[i].dest.sendValue(price);
+    //             totalAmount = totalAmount + price;
+    //         }
+    //     }
+
+    //     console.log("totalAmount", totalAmount);
+
+    //     // Revert if the wrong amount of ETH was sent
+    //     require(
+    //         total == totalAmount,
+    //         "bulkPayment: Too much ETH sent"
+    //     );
+    // }
 
     /**
      * @notice Transfers all tokens of the input adress to the recipient. This is
